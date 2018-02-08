6bbc4b71d66d4aece8a04d0438523a1efb0269f2
Andrea Lara <anlara@csumb.edu>
2018-02-08 07:33:50 +0000

    Add all hw1 files

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+    <!--head -->
+    <head>
+        <meta charset="utf-8" />
+        <title>Operating Systems</title>
+        <link href="CSS/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+    </head>
+    <!-- body -->
+    <body>
+        <header>
+            <h1>
+                Operating Systems
+            </h1>
+        </header>
+        <div class="topnav">
+            <hr width= "50%" />
+            <a href= "index.php" >Home</a>
+            <a href= "types.html">Types</a>
+            <a href= "user.html">User</a> 
+            <a href= "differences.html">Differences</a>
+        </div>
+        
+        <main>
+            <figure id="me">
+                <br>
+                <img src ="img/opsystems.png"alt="OpSystem"/>
+                </br>
+            </figure>
+            
+            <div id="main">
+                
+                <div class="row">
+                    <div clss="column">
+                        <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+                    </div>
+                </div>
+                <h2>
+                <h5><strong>What is an Operating System?</strong> <br /></h5>
+               <p>
+                   An operating system can be viewed as the software that makes 
+                   <br>your devices function such as: cell phones,desktops, laptops.</br>
+               </p>
+               <font size="5", color="orange">Functionality of Operating Systems</font>
+               <p>
+                   The OS controls all of the software and hardware on a computer. 
+                   <br>A computer often times has multiple things running on the background
+                   even when initially starting up a computer.</br>
+                   The software allow for all the computer programs running in the background
+                   <br></b>to access the CPU,memory and storage. Enabling it to gather the appropriate information.</br>
+                   </p>
+                   <br /><br />
+                   <em>"Computer Science is the operating system for all <strong> Innovation</strong>"</em>
+                   <br>- <a href="https://www.forbes.com/profile/steve-ballmer/">Steve Ballmer</a></br>
+            </div>
+        </h2>    
+        </main>
+        <footer>
+            <hr>
+            <a href="https://www.gcflearnfree.org/computerbasics/understanding-operating-systems/1/">Understanding Operating Sytems</a>
+            <br>
+                <font size="4", color="black">Internet Programming. 2017&copy;Lara</font>
+            </br>
+        </footer>
+        
+        <head>
+            <meta charset= "utf-8"/>
+            <title>Operating Systems</title>
+            
+            <style>
+                body{
+                    background-color: #2471a3;
+                }
+            </style>
+        </head>
+        
+    </body>
+    
+</html>
